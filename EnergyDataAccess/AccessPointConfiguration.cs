6c9b133fdcc7ec4c54131636e247e3df65999130
Maxime Visconte <maxime@visconte.fr>
2022-01-07 21:54:40 +0100

    use config file

---
+++ EnergyDataAccess/AccessPointConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace DatabaseUpdate
+{
+    internal record AccessPointConfiguration
+    {
+        public string AccessPointId { get; init; }
+
+        public EnergyDataAccess.EnergyEnum Energy { get; init; }
+    }
+}
